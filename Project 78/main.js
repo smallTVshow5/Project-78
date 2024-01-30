a76893c4283d0aad0aaa0b8e8c6d67d848c899c9
smallTVshow5 <152315851+smallTVshow5@users.noreply.github.com>
2024-01-30 22:02:17 +0400

    Add files via upload

---
+++ Project 78/main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://pluspng.com/png-mother-only-4170.html" ]
+var names = ["Famaily Book","Pravin Balbheem Kamble", "Pranish Pravin Kamble", "Priyansha Pravin Kamble", "Manisha Pravin Kamble"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 4
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i] ;
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
